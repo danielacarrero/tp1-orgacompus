43f3ea660797655a8d7e11e4aa121d30ec464369
Ariii1996 <ariel.piro@hotmail.com>
2019-09-30 21:11:31 -0300

    Add files via upload

--- src/asm-helpers.c
+++ src/asm-helpers.c
@@ -1,7 +1,14 @@
 #include "builders.h"
+#include "ant_engine.h"
 
 void 
 doPanic(char *s)
 {
   panic(s);
+}
+
+void
+doAdjust(uint32_t *p ,uint32_t v, uint32_t limit)
+{
+  adjust(p, v, limit);
 }
