94d3db79c2257142fb6c5958052ec173fe9b865a
Ariel <ariel.piro@hotmail.com>
2019-11-06 19:52:31 -0300

    agregando

--- tp2-cache/src/main.c
+++ tp2-cache/src/main.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include "cache.h"
 
 char **splitline(char* line){
 
-    
+    char **words;
+    char *found;
+    int i = 0;
+
+    while((found = strsep(&line, " ,\n")) != NULL){
+        if (strcmp(found, "") == 0) continue;
+        words[i] = found;
+        i++;
+    }
+
+    return words;
 }
 
 int main(int argc, char** argv) {
 
     if(argc != 2){
         printf("No se ingreso ningun archivo\n");
-        return -1;
+        exit(-1);
     }
 
     if (create_cache_and_memory() < 0){
         printf("Error creando cache y memoria principal\n");
-        return -1;
+        exit(-1);
     }
 
     FILE *comandos;
     comandos = fopen(argv[1], "r");
     if(!comandos){
         printf("No se pudo abrir el archivo\n");
-        fclose(comandos);
-        return -1;
+        exit(-1);
     }
 
     char line[20];
@@ -41,36 +50,12 @@ int main(int argc, char** argv) {
         }
         else if(strcmp(words[0], "R") == 0){
 
-            if(sizeof(words) != 2) {
-                printf("Error introduciendo comando R\n");
-                fclose(comandos);
-                return -1;
-            }
-
-            if (sizeof(words[1]) > 5){
-                printf("Error introduciendo comando R\n");
-                fclose(comandos);
-                return -1;
-            }
-
             unsigned int address = atoi(words[1]);
-
             unsigned char byte = read_byte(address);
+
             printf("El valor del byte leido es de: %c\n", byte);
         }
         else if(strcmp(words[0], "W") == 0){
-           
-            if(sizeof(words) != 3) {
-                printf("Error introduciendo comando W\n");
-                fclose(comandos);
-                return -1;
-            }
-
-            if (sizeof(words[1]) > 5 || sizeof(words[2]) > 3){
-                printf("Error introduciendo comando W\n");
-                fclose(comandos);
-                return -1;
-            }
 
             unsigned int address = atoi(words[1]);
             unsigned int value = atoi(words[2]);
@@ -87,7 +72,7 @@ int main(int argc, char** argv) {
         }else{
             printf("Error introduciendo comando\n");
             fclose(comandos);
-            return -1;
+            exit(-1);
         }
 
         fgets(line, 20, comandos);
@@ -95,5 +80,5 @@ int main(int argc, char** argv) {
     
     fclose(comandos);
 
-    return 0;
+    exit(0);
 }
